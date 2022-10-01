e20a2959215f45a24cd9ba14dc27e7423a81c6fc
Raif <80786334+raifbrah@users.noreply.github.com>
2022-10-01 16:20:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function addTodoBtn_changeTop() {
   addTodoBtn.style.top = '100%'
 }
 function addTodoWindow_changeTop() {
-  addTodoWindow.style.top = '100%'
+  addTodoWindow.style.top = '100vh'
 }
 
 addTodoBtn_changeTop()
