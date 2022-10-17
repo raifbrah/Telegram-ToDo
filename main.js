5e722d9ca9d4ff491f2ff523e47cec2f9544fa2d
Raif <80786334+raifbrah@users.noreply.github.com>
2022-10-17 11:32:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -76,3 +76,27 @@ function addTodoWindow_changeTop() {
 addTodoBtn_changeTop()
 addTodoWindow_changeTop()
 
+
+// отключение zoom через скролл (в том числе трекападами в macOS)
+document.addEventListener('mousewheel', function(e){
+  if(!e.ctrlKey && !e.metaKey) return;
+
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
+
+// отключение zoom прикосновениями (в том числе трекападами и т.п.) в Safari и iOS
+document.addEventListener('gesturestart', function(e){
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
+
+// отключение zoom через клавиатуру (ctrl + "+", ctrl + "-")
+// кнопки браузера для управления zoom отключены не будут
+document.addEventListener('keydown', function(e){
+  if(!e.ctrlKey && !e.metaKey) return;
+  if(e.keyCode != 189 && e.keyCode != 187) return;
+
+  e.preventDefault();
+  e.stopImmediatePropagation();
+}, {passive:false});
